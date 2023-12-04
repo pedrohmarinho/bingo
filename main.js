1b73a505af00c6e1bb0918af8273441c9bef5804
pedromarinho262@gmail.com <pedromarinho262@gmail.com>
2023-12-03 23:03:47 -0300

    Adicionando melhorias e estilizando o jogo

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const generateButton = document.getElementById('generate-board');
-const callButton = document.getElementById('call-number');
 const generateAllButton = document.getElementById('generate-all-numbers');
 const restartButton = document.getElementById('restart-game');
 const board = document.getElementById('bingo-board');
-const player1Board = document.getElementById('player1-board');
-const player2Board = document.getElementById('player2-board');
+const playersContainer = document.getElementById('players-container');
 
 let numbersCalled = [];
-let currentPlayer = 1;
+let players = [];
 
 function generateRandomNumber(usedNumbers) {
   let number;
@@ -17,7 +15,8 @@ function generateRandomNumber(usedNumbers) {
   return number;
 }
 
-function generateBoard(playerBoard) {
+function generateBoard(player) {
+  const playerBoard = document.getElementById(`player${player.id}-board`);
   playerBoard.innerHTML = '';
   const usedNumbers = [];
   for (let i = 0; i < 24; i++) {
@@ -34,79 +33,91 @@ function toggleActiveSquare(square) {
   square.classList.toggle('active');
 }
 
-function checkWinner(playerBoard, playerName) {
+function checkWinner(player) {
+  const playerBoard = document.getElementById(`player${player.id}-board`);
   const squares = playerBoard.querySelectorAll('.board-item');
   const markedSquares = Array.from(squares).filter((square) =>
     square.classList.contains('active')
   );
 
-  if (markedSquares.length === 24) {
-    alert(`Parabéns, ${playerName}! Você ganhou o jogo!`);
-    restartGame();
-  }
+  return markedSquares.length === 24;
 }
 
-function callNumber() {
-  const number = generateRandomNumber(numbersCalled);
-  numbersCalled.push(number);
-  alert(`Número chamado: ${number}`);
-
-  const squares = document.querySelectorAll('.board-item');
-  squares.forEach((square) => {
-    if (parseInt(square.innerText) === number) {
-      toggleActiveSquare(square);
+function checkForWinner() {
+  for (let i = 0; i < players.length; i++) {
+    if (checkWinner(players[i])) {
+      alert(`Parabéns, ${players[i].name}! Você ganhou o jogo!`);
+      restartAndBackToHome();
+      return true;
     }
-  });
-
-  checkWinner(player1Board, 'Jogador 1');
-  checkWinner(player2Board, 'Jogador 2');
-
-  switchPlayer();
+  }
+  return false;
 }
 
 function generateAllNumbers() {
   if (numbersCalled.length === 75) {
     alert('Todos os números já foram chamados. Fim de jogo!');
-    restartGame();
+    restartAndBackToHome();
   } else {
-    for (let i = numbersCalled.length + 1; i <= 75; i++) {
-      numbersCalled.push(i);
+    const table = document.getElementById('called-numbers');
+    const intervalId = setInterval(() => {
+      if (checkForWinner()) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      const number = generateRandomNumber(numbersCalled);
+      numbersCalled.push(number);
+
+      const newRow = table.insertRow(-1);
+      const cell1 = newRow.insertCell(0);
+      cell1.innerHTML = number;
+
+      players.forEach(player => {
+        const squares = document.querySelectorAll(`#player${player.id}-board .board-item`);
+        squares.forEach((square) => {
+          if (parseInt(square.innerText) === number) {
+            toggleActiveSquare(square);
+          }
+        });
+      });
+
+      if (numbersCalled.length === 75) {
+        alert('Todos os números foram chamados!');
+        clearInterval(intervalId);
+        restartAndBackToHome();
+      }
+    }, 200); // Intervalo de 700 milissegundos (0.7 segundos) entre cada número
+  }
+}
+
+function restartAndBackToHome() {
+  location.reload();
+}
+
+function initializeGame() {
+  const numPlayers = prompt('Digite o número de jogadores (entre 2 e 4):');
+
+  if (numPlayers >= 2 && numPlayers <= 4) {
+    for (let i = 1; i <= numPlayers; i++) {
+      const playerName = prompt(`Digite o nome do jogador ${i}:`);
+      const player = { id: i, name: playerName };
+      players.push(player);
+      const playerDiv = document.createElement('div');
+      playerDiv.className = 'player';
+      playerDiv.innerHTML = `
+        <h2>${playerName}</h2>
+        <div id="player${i}-board" class="player-board"></div>
+      `;
+      playersContainer.appendChild(playerDiv);
+      generateBoard(player);
     }
-    alert('Todos os números foram chamados!');
-    checkWinner(player1Board, 'Jogador 1');
-    checkWinner(player2Board, 'Jogador 2');
-    switchPlayer();
-  }
-}
-
-function switchPlayer() {
-  currentPlayer = currentPlayer === 1 ? 2 : 1;
-}
-
-function restartGame() {
-  generateBoard(player1Board);
-  generateBoard(player2Board);
-  numbersCalled = [];
-  currentPlayer = 1;
-}
-
-generateButton.addEventListener('click', () => {
-  restartGame();
-});
-
-callButton.addEventListener('click', () => {
-  if (numbersCalled.length < 75) {
-    callNumber();
+    generateButton.disabled = true;
   } else {
-    alert('Todos os números já foram chamados. Fim de jogo!');
-    restartGame();
+    alert('Número de jogadores inválido. O jogo requer entre 2 e 4 jogadores.');
   }
-});
+}
 
-generateAllButton.addEventListener('click', () => {
-  generateAllNumbers();
-});
-
-restartButton.addEventListener('click', () => {
-  restartGame();
-});
+generateButton.addEventListener('click', initializeGame);
+generateAllButton.addEventListener('click', generateAllNumbers);
+restartButton.addEventListener('click', restartAndBackToHome);
